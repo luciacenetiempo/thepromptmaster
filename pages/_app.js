6c5fa0a6e98f6b6f6544f98f390a0aa1b0d389dd
luciacen <luciacenetiempo@gmail.com>
2023-11-24 13:19:35 +0100

    cookie policy

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,46 @@
 import '@/styles/globals.css'
 import Script from 'next/script';
+import CookieBanner from '@/components/CookieBanner';
+import { useCookies } from 'react-cookie';
+
+import React, { useState, useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
-  return (<>
+  const [cookies, setCookie, removeCookie] = useCookies();
+  const [hasCookie, setHasCookie] = useState(false);
+  useEffect(() => {
+    if (cookies.CookieAccepted) {
+      setHasCookie(true)
+    }
+  }, [cookies]);
 
-    <Script
-      src="https://www.googletagmanager.com/gtag/js?id=G-QXT3DWCP13"
-      strategy="afterInteractive"
-    />
-    <Script
-      async
-      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2302004936713476"
-      crossorigin="anonymous"
-    />
-    <Script id="google-analytics" strategy="afterInteractive">
-      {`
+  return hasCookie ? (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-QXT3DWCP13"
+        strategy="afterInteractive"
+      />
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2302004936713476"
+        crossorigin="anonymous"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
            window.dataLayer = window.dataLayer || [];
            function gtag(){dataLayer.push(arguments);}
            gtag('js', new Date());
          
            gtag('config', 'G-QXT3DWCP13');
         `}
-    </Script>
-    <Component {...pageProps} />
-  </>)
+      </Script>
+      <Component {...pageProps} />
+    </>
+  ) : (
+    
+    <>
+      <CookieBanner />
+      <Component {...pageProps} />
+    </>
+  )
 }
