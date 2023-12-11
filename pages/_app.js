ee2b0e49c7847314ea786d4213c10d6cb46498be
luciacen <luciacenetiempo@gmail.com>
2023-12-11 15:08:19 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -26,7 +26,7 @@ export default function App({ Component, pageProps }) {
         crossorigin="anonymous"
       />
       <Component {...pageProps} />
-      {hasCookie ? (<CookieBanner />) : ('')}
+      {!hasCookie ? (<CookieBanner />) : ('')}
     </>
   )
   
