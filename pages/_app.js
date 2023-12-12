c8a702720b857469e0b10a315d0bcad6428da8eb
luciacen <luciacenetiempo@gmail.com>
2023-12-12 11:42:20 +0100

    GA

--- pages/_app.js
+++ pages/_app.js
@@ -17,6 +17,30 @@ export default function App({ Component, pageProps }) {
   return (
 
     <>
+    <Script>
+      {`
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-PNQFCZCN');
+      `}
+    </Script>
+    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-QXT3DWCP13"></Script>
+    <Script id="google-analytics">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-QXT3DWCP13');
+      `}
+    </Script>
+    <Script
+      async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2302004936713476"
+      crossorigin="anonymous"
+    />
       <Component {...pageProps} />
       {!hasCookie ? (<CookieBanner />) : ('')}
     </>
--- pages/_document.js
+++ pages/_document.js
@@ -4,30 +4,6 @@ export default function Document() {
   return (
     <Html lang="it">
       <Head>
-        <Script>
-          {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-PNQFCZCN');
-          `}
-        </Script>
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-QXT3DWCP13"></Script>
-        <Script id="google-analytics">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-          
-            gtag('config', 'G-QXT3DWCP13');
-          `}
-        </Script>
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2302004936713476"
-          crossorigin="anonymous"
-        />
       </Head>
       <body>
         <noscript>
