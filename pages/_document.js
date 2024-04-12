3fd22ecd0d153ce2e15ab76e81b8e904b30476b8
Lucia <38978846+luciacenetiempo@users.noreply.github.com>
2024-04-12 18:03:58 +0200

    kk

--- pages/_document.js
+++ pages/_document.js
@@ -6,18 +6,6 @@ export default function Document() {
       <Head>
       </Head>
       <body>
-        <noscript>
-          <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PNQFCZCN"
-            height="0" 
-            width="0" 
-            style={{
-              display: "none",
-              visibility: "hidden"
-            }}
-            >
-          </iframe>
-        </noscript>
         <Script src="https://groot.mailerlite.com/js/w/webforms.min.js?v2d8fb22bb5b3677f161552cd9e774127" type="text/javascript"></Script>
         <Script>
           fetch("https://assets.mailerlite.com/jsonp/863030/forms/115595877754603214/takel")
