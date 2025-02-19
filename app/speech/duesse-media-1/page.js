9c9de9edcd10144afd8b631186cfc2d72a99a96e
Lucia <38978846+luciacenetiempo@users.noreply.github.com>
2025-02-19 01:53:21 +0100

    Update page.js

--- app/speech/duesse-media-1/page.js
+++ app/speech/duesse-media-1/page.js
@@ -64,7 +64,7 @@ export default function Page() {
         </div>
       </div>
       <div className=''>
-        {/* <div className="strip">
+        {/* <div className="strip"> 
           <LoopingText
             text='THE PROMPT MASTER'
             size='big'
