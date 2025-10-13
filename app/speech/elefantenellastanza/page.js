8757c254ba300b68e96c11f8b3dcd7b94d712fa7
Lucia <38978846+luciacenetiempo@users.noreply.github.com>
2025-10-13 23:32:41 +0200

    20251014-elefantenellastanza

---
+++ app/speech/elefantenellastanza/page.js
@@ -0,0 +1,77 @@
+import Header from '/components/Header'
+import HeaderPlain from '../../../components/HeaderPlain'
+import LoopingText from '../../../components/LoopingText'
+import NewsletterFormSlide from '../../../components/NewsletterFormSlide';
+
+export const metadata = {  
+  title: "Elefante nella stanza - The Prompt Master",
+  description: "L'intelligenza artificiale come non te l'aspettavi",
+  metadataBase: new URL('https://www.thepromptmaster.it'),
+  alternates: {
+    canonical: '',
+  },
+  openGraph: {
+    images: '/og-image.png',
+  },
+  keywords: ['ChatGPT', 'Midjourney', 'Chat GPT', 'Prompt', 'Prompt Engineering', 'Prompt Design', 'Prompt ChatGPT', 'Intelligenza Artificiale'],
+  authors: [{ name: 'Lucia Cenetiempo', url: 'https://www.thepromptmaster.it' }],
+}
+
+export default function Page() {
+  return (
+    <>
+      <div className='headMarginSmall nomarginTop'>
+        <div className="strip">
+          <LoopingText
+            text='THE PROMPT MASTER'
+            size='big'
+            velocity={0.08}
+            color='dark'
+          />
+        </div>
+        {/* <div className="strip">
+          <LoopingText
+            text={`newsletter`}
+            size='small'
+            velocity={0.08}
+            color='dark'
+          />
+        </div> */}
+      </div>
+
+      <div className='section-content fullheight'>
+        <div className='content rich-text-block rich-text-block--newsletter'>
+          <p className='tx-center'><strong>Inserisci la tua email<br/>e scarica le slide</strong></p>
+          <h2 className='tx-center'>Il potere dell'<br/>AI GENERATIVA</h2>
+          <NewsletterFormSlide 
+            listId='143770881076757629' 
+            label='Inserisci il tuo indirizzo email!' 
+            submit='Iscriviti!' 
+            success=''
+            error=''
+            speechName='Confindustria Bari'
+            slideLink='/file/elefantenellastanza/20251014-elefantenellastanza.pdf'
+          />
+        </div>
+      </div>
+      <div className=''>
+        <div className="strip">
+          <LoopingText
+            text='THE PROMPT MASTER'
+            size='big'
+            velocity={0.08}
+            color='dark'
+          />
+        </div>
+        {/* <div className="strip">
+          <LoopingText
+            text={`AI WEEK`}
+            size='small'
+            velocity={0.08}
+            color='dark'
+          />
+        </div> */}
+      </div>
+    </>
+  );
+}
